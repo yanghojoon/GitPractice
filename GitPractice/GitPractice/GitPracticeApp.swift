223f33383ff1511b2e4d51ac65bb73634ef6fc2e
yanghojoon <yanghojoon8487@gmail.com>
2023-06-24 13:40:57 +0900

    chore: 예제 프로젝트 생성

---
+++ GitPractice/GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by 양호준 on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
